9b0913c2a2c0a3edc09b533891ea3b4aa9e31c28
matt <fournier.matthew@gmail.com>
2014-05-10 22:58:54 -0400

    refactored data frame manipulation

--- plot3.R
+++ plot3.R
@@ -4,12 +4,6 @@ plot3 <- function(DF=NULL) {
 		source("read_data.R")
 		DF <- read_data()
 	}
-
-	#I searched for a while trying to figure out how to add the weekday names
-	#to the axes ticks. It turns out that if you represent the date and time
-	#in POSIX format and plot, then R takes care of that for you. Which
-	#is like Christmas, that's awesome. 
-	DF$DateTime <- as.POSIXct(paste(DF$Date, DF$Time), format="%Y-%m-%d %H:%M:%S")
 	
 	plot(DF$DateTime, DF$Sub_metering_1, type="l", col="black", yaxp=c(0, 30, 3),
 		  	xlab="",
@@ -19,7 +13,7 @@ plot3 <- function(DF=NULL) {
 	points(DF$DateTime, DF$Sub_metering_3, type="l", col="blue")
 
 	legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1))
-	
+
 	dev.copy(png, "plot3.png")
 	dev.off()
 }
--- read_data.R
+++ read_data.R
@@ -12,5 +12,11 @@ read_data <- function() {
   df <- subset(df, df$Date >= "2007-02-01")
   df <- subset(df, df$Date <= "2007-02-02")
   
+  #I searched for a while trying to figure out how to add the weekday names
+  #to the axes ticks. It turns out that if you represent the date and time
+  #in POSIX format and plot, then R takes care of that for you. Which
+  #is like Christmas, that's awesome. 
+  df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format="%Y-%m-%d %H:%M:%S")
+
   df
 }
